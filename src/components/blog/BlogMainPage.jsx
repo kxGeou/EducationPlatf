e2ef0d50abf3f8e4f5d016b8b90980ae184df9c6
kxGeou <grzegorzm.work@gmail.com>
2025-07-20 00:09:16 +0200

    Add ImageSection component to blog main page
    Introduced a new ImageSection component with associated CSS for a clipped background effect. Integrated ImageSection and Footer into BlogMainPage to enhance the page layout and visual design.

--- src/components/blog/BlogMainPage.jsx
+++ src/components/blog/BlogMainPage.jsx
@@ -2,20 +2,34 @@ import React from 'react'
 import RedHeader from '../homepage/RedHeader';
 import HeroBlog from './HeroBlog';
 import CourseListHero from '../homepage/CourseListHero';
+import Footer from '../homepage/Footer';
 import BlogList from './BlogList';
 import AboutBlogs from './AboutBlogs';
+import ImageSection from './ImageSection';
+
+
 function BlogMainPage() {
   return (
-    <div className='flex justify-center min-h-screen bg-gradient-to-br to-35% from-secondaryGreen to-white'>
-        <RedHeader></RedHeader>
-        <div className='w-full max-w-[1100px] mt-26 lg:mt-38'>
-            <HeroBlog></HeroBlog>
-            <CourseListHero></CourseListHero>
-            <BlogList></BlogList>
-            <AboutBlogs></AboutBlogs>
-        </div>
+    <div className='flex flex-col items-center min-h-screen bg-gradient-to-br to-35% from-secondaryGreen to-white'>
+      <RedHeader />
+      
+      <div className='w-full max-w-[1100px] mt-26 lg:mt-38'>
+        <HeroBlog />
+        <CourseListHero />
+        <BlogList />
+        <AboutBlogs />
+      </div>
+
+      <div className='w-full flex items-center justify-center'>
+        <ImageSection />
+      </div>
+
+      <div className='w-full max-w-[1100px]'>
+        <Footer />
+      </div>
     </div>
   )
 }
+
 
 export default BlogMainPage
---
+++ src/components/blog/ImageSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../../styles/ImageSection.css';
+function ImageSection() {
+  return (
+    <div className='bg-gradient-to-br from-secondaryBlue to-primaryBlue text-white mt-36 md:mt-0 w-full max-w-[1920px] imageSection mb-12 min-h-screen px-6 flex items-center justify-center'>ImageSection</div>
+  )
+}
+
+export default ImageSection
