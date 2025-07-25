02be8d6e406e24401c4a7c36d9ffef122de5350e
kxGeou <grzegorzm.work@gmail.com>
2025-07-25 19:36:27 +0200

    Remove arrow animation from SectionHeading
    Deleted ArrowAnimation.css and removed the related import and class from SectionHeading.jsx, eliminating the animated effect on the ChevronRight icon.

--- src/components/typography/SectionHeading.jsx
+++ src/components/typography/SectionHeading.jsx
@@ -1,10 +1,9 @@
 import { ChevronRight } from "lucide-react";
-import '../../styles/arrowAnimation.css';
 const SectionHeading = ({ children, textColor }) => (
   <span
     className={`md:text-lg font-semibold flex items-center gap-1 ${textColor} mb-3 `}
   >
-    <ChevronRight className="arrowAnimation" size={20}></ChevronRight>
+    <ChevronRight size={20}></ChevronRight>
     {children}
 
   </span>
